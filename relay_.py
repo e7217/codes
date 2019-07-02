bb130635961a1f67ef8017b89fd3361c4b14007a
Changyong Um <e7217@naver.com>
2019-07-02 14:17:21 +0900

    upload relay code

---
+++ relay_.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+# port = '/dev/ttyUSB0'
+port = 'com9'
+
+code = 'A00101A2'
+code2 = 'A00100A1'
+
+def init_(port, code):
+    ser = serial.Serial(port, 9600, timeout=1)
+    ser.write(code.decode('hex'))
+    ser.close()
+
+settime = 1
+
+while 1:
+    try :
+        init_(port,code)
+        time.sleep(settime)
+        init_(port, code2)
+        time.sleep(settime)
+    except:
+        print 'out'

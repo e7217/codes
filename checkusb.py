dcbe0ae5866e12d5d6c8b562408139fcbf494c8c
Changyong Um <e7217@naver.com>
2020-04-22 14:24:04 +0900

    logging : added logging method in checkusb.py

--- checkusb.py
+++ checkusb.py
@@ -1,12 +1,47 @@
 from serial.tools import list_ports as lp
+import logging, os
+from logging.handlers import RotatingFileHandler
+
+# logging.basicConfig(
+#     filename='logtest.log',
+#     filemode='a',
+#     format='(%(asctime)s) [ %(levelname)s ] : %(message)s',
+#     datefmt='%m/%d/%Y %I:%M:%S %p',
+#     level=logging.DEBUG)
+#
+logger = logging.getLogger('test logger')
+logger.setLevel(logging.DEBUG)
+
+# logger.disabled = True
+
+# 10MB
+fileMaxByte = 1024*1024*10
+
+fh = RotatingFileHandler('filelog.log', maxBytes=fileMaxByte, backupCount=2)
+fh.setLevel(0)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 def checkUSB():
     lplist = lp.comports('/dev/')
-
+    logger.info(' [ check connection ] ')
     for i in lplist:
         if i.device == '/dev/ttyUSB0':
+            logger.info('connection : True')
             return True
         else :
+            logger.warning('connection : False')
+            # os.system("sudo reboot")
             return False
 
-print 'USB CONNECTION : ', checkUSB()
+checkUSB()
+
+

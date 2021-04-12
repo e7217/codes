0140f37c5c3a3cbd2e8bfb7caf7ea554621f6782
Changyong Um <e7217@naver.com>
2021-04-12 13:35:57 +0900

    update logcode

--- logcode/main.py
+++ logcode/main.py
@@ -1,24 +1,26 @@
 import os, logging, logging.config, yaml
 
+
 def createFolder(directory):
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
     except OSError:
-        print('Error: Creating directory. ' + directory)
+        print("Error: Creating directory. " + directory)
 
-log_dir_lists = ['./logs', './logs_date']
-for dir in log_dir_lists:
-    createFolder(dir)
 
-with open('config.yaml', 'rb') as fr:
+log_dir_lists = ["./logs", "./logs_date"]
+for log_dir in log_dir_lists:
+    createFolder(log_dir)
+
+with open("config.yaml", "rb") as fr:
     config = yaml.load(fr, Loader=yaml.FullLoader)
     logging.config.dictConfig(config)
 
-logger = logging.getLogger('simpleExample')
+logger = logging.getLogger("simpleExample")
 
-logger.debug('debug')
-logger.info('info')
-logger.warning('warning')
-logger.error('error')
-logger.critical('critical')
+logger.debug("debug")
+logger.info("info")
+logger.warning("warning")
+logger.error("error")
+logger.critical("critical")
